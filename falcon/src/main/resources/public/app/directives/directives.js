3b87a1d3da02cba9bf61807d870d6256b43acf64
Ivan Rafael Lampert <irlampert1@stefanini.com>
2016-06-12 19:13:35 -0300

    #13: Modal de Confirmação Genérica

--- falcon/src/main/resources/public/app/directives/directives.js
+++ falcon/src/main/resources/public/app/directives/directives.js
@@ -14,5 +14,22 @@
 			};
 		})
 		
+		.directive('dialogconfirm', function() {
+			return {
+				scope : {
+					modalid: '@',
+					text: '@',
+					action: '&'
+				},
+				restrict : 'E',
+				templateUrl: '/views/directive-templates/dialog-confirm-template.html',
+				link : function(scope, elem) {
+					scope.fireConfirmAction = function() {
+						scope.action();
+					}
+				}
+			};
+		})
+		
 		;
 })();
