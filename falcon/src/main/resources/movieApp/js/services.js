8632acdc2dd4312d11c08b6ec309a417af374443
Ivan Rafael Lampert <irlampert1@stefanini.com>
2016-06-12 19:18:36 -0300

    Removido diretorio de exemplos

--- falcon/src/main/resources/movieApp/js/services.js
+++
@@ -1,7 +0,0 @@
-angular.module('movieApp.services', []).factory('Movie', function($resource) {
-  return $resource('localhost/#/movies/:id', { id: '@_id' }, {
-    update: {
-      method: 'PUT'
-    }
-  });
-});
