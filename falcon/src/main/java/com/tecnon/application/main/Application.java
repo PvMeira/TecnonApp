13123923a76b6a069eb4bd4cde493d7f7a692190
Ivan Rafael Lampert <irlampert1@stefanini.com>
2016-06-13 14:43:38 -0300

    Ajuste de css no menu

--- falcon/src/main/java/com/tecnon/application/main/Application.java
+++ falcon/src/main/java/com/tecnon/application/main/Application.java
@@ -1,22 +1,14 @@
 package com.tecnon.application.main;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.tecnon.domain.entity.Foo;
-import com.tecnon.domain.repository.FooRepository;
 
 /**
  * @author irlampert1
@@ -32,8 +24,6 @@ import com.tecnon.domain.repository.FooRepository;
 @SuppressWarnings("all")
 public class Application {
 
-//	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-	
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 //        listBeanNames(ctx);
