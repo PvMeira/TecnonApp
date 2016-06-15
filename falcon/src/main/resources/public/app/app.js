e559f60aab4765f14b0da9588dfdb44709307787
Ivan Rafael Lampert <irlampert1@stefanini.com>
2016-06-15 07:12:08 -0300

    Ajuste no app.js

--- falcon/src/main/resources/public/app/app.js
+++ falcon/src/main/resources/public/app/app.js
@@ -3,36 +3,12 @@
 
 	angular
 		.module('app',['ngRoute',  'ngResource', 'ngMessages'])
-//		.config(['$routeProvider', '$httpProvider', viewRouter])
 		.controller('Home', ['$http', homeController])
         .factory('GlobalService', ['$location', GlobalService])
         .run(function($rootScope, GlobalService) {
             $rootScope.service = GlobalService;  
         }); 		
 
-//	function viewRouter($routeProvider, $httpProvider){
-//		$routeProvider
-//		.when('/', {
-//			templateUrl : 'views/home.html',
-//			controller : 'Home as controller'
-//		})
-//		.when('/userlist', {
-//			templateUrl : 'views/user/userList.html',
-//			controller : 'UserListController as controller'
-//		})	
-//		.when('/usernew', {
-//			templateUrl : 'views/user/userAddForm.html',
-//			controller : 'UserAddController as controller'
-//		})	
-//		.when('/useredit/:id', {
-//			templateUrl : 'views/user/userEditForm.html',
-//			controller : 'UserEditController as controller'
-//		})	
-//		.otherwise('/');
-//		
-//		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';		
-//	}	
-	
 	function homeController($http){
 		var self = this;
 	}
